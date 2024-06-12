2541ce4fae1e27a5c767ab1ae8bb53ce42d63176
Tom Fyfe <tomfyfe85@gmail.com>
2024-06-12 18:49:48 +0100

    passing children

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 function App() {
   let items = ["New York", "San Fran", "Tokyo", "London", "Paris"];
   const onSelectedItem = (item: string) => {
     console.log(item);
   };
   return (
-    <div>
-      <ListGroup items={items} heading="Cities" onSelectedItem={onSelectedItem}/>
-    </div>
+    <>
+      <Alert>
+        Hello <span><h1>WORLD</h1></span>
+      </Alert>
+    </>
   );
 }
 
