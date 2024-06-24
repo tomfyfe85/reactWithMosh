11d16d88e5bd450f699f352bda9c494ade7dc533
Tom Fyfe <tomfyfe85@gmail.com>
2024-06-24 13:10:45 +0100

    'controlled components complete'

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,23 +1,46 @@
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 
 const Form = () => {
+  const [person, setPerson] = useState({
+    name: "",
+    age: "",
+  });
+
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
     console.log("Submitted");
+    console.log(person);
   };
+
   return (
     <form onSubmit={handleSubmit}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input id="name" type="text" className="form-control" />
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, name: event.target.value })
+          }
+          value={person.name}
+          id="name"
+          type="text"
+          className="form-control"
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input id="age" type="number" className="form-control" />
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, age: parseInt(event.target.value) })
+          }
+          value={person.age}
+          id="age"
+          type="number"
+          className="form-control"
+        />
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
