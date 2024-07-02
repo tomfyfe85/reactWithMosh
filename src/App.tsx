cefc598594a46da80edcfcab119db3cb76a096c3
Tom Fyfe <tomfyfe85@gmail.com>
2024-07-02 22:59:02 +0100

    understanding useEffect complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ExpandableText from "./components/ExpandableText";
 import Form from "./components/Form";
 function App() {
+  const ref = useRef<HTMLInputElement>(null);
+  
+  // afterRender
+  useEffect(() => {
+    if (ref.current) ref.current.focus();
+  });
+
+  useEffect(() => {
+    document.title = "My App";
+  });
   return (
     <div>
-      <Form/>
+      <input ref={ref} type="text" className="form-control" />
     </div>
   );
 }
