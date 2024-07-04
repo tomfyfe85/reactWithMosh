f91a34ea76cc41c109305203c5a78ce9457aa431
Tom Fyfe <tomfyfe85@gmail.com>
2024-07-04 11:14:45 +0100

    Fetching data complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
-import { useEffect  } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { number } from "zod";
+
+interface User{
+  id: number,
+  name: string 
+}
+
 function App() {
-  const connect = () => console.log("Connecting");
-  const disconnect = () => console.log("Disconnecting");
-  
+  const [users, setUsers] = useState<User[]>([]);
+
   useEffect(() => {
-    connect()
-    return () => disconnect()
-  })
-  return (
-    <div>
-    
-    </div>
-  );
+    axios.get<User[]>("https://jsonplaceholder.typicode.com/users")
+    .then(res => setUsers(res.data));
+  }, []);
+
+  return <ul>
+    {users.map((user) => 
+      <li key={user.id}>{user.name}</li>
+    )}
+  </ul>;
 }
 
 export default App;
