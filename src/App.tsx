06a27c092f285c82b365cbd0bda0ff6708c7a27e
Tom Fyfe <tomfyfe85@gmail.com>
2024-07-04 10:46:58 +0100

    Effect clean up complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
-import { useEffect, useRef, useState } from "react";
-import ExpandableText from "./components/ExpandableText";
-import Form from "./components/Form";
-import ProductList from "./ProductList";
+import { useEffect  } from "react";
 function App() {
-  const [category, setCategory] = useState("");
-
+  const connect = () => console.log("Connecting");
+  const disconnect = () => console.log("Disconnecting");
+  
+  useEffect(() => {
+    connect()
+    return () => disconnect()
+  })
   return (
     <div>
-      <select
-        title="select"
-        className="form-select"
-        onChange={(event) => setCategory(event.target.value)}
-      >
-        <option value=""></option>
-        <option value="Clothing">Clothing</option>
-        <option value="Household">Household</option>
-      </select>
-      <ProductList category={category} />
+    
     </div>
   );
 }
