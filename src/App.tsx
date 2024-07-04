bb368e909470697ffe46768652cfed2cb2b0b34f
Tom Fyfe <tomfyfe85@gmail.com>
2024-07-04 11:56:05 +0100

    handling errors complete

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,32 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { number } from "zod";
 
-interface User{
-  id: number,
-  name: string 
+interface User {
+  id: number;
+  name: string;
 }
 
 function App() {
   const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
 
   useEffect(() => {
-    axios.get<User[]>("https://jsonplaceholder.typicode.com/users")
-    .then(res => setUsers(res.data));
+    axios
+      .get<User[]>("x")
+      .then((res) => setUsers(res.data))
+      .catch((err) => setError(err.message));
   }, []);
 
-  return <ul>
-    {users.map((user) => 
-      <li key={user.id}>{user.name}</li>
-    )}
-  </ul>;
+  return (
+    <>
+      {error && <p className="text-danger">{error}</p>}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
