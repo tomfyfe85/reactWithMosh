f615ca7e38e322a7d179dac7bed5a19fb922b576
tomfyfe <tomfyfe85@gmail.com>
2025-07-18 10:24:17 +0100

    passing functins as props

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,9 +1,15 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  let items = ["New York", "San Fran", "Toykyo", "London"];
+
   return (
     <div>
-      <ListGroup />
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={(item: string) => console.log(item)}
+      />
     </div>
   );
 }
--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,18 +1,24 @@
 import { useState } from "react";
 
-function ListGroup() {
-  let items = ["New York", "San Fran", "Toykyo", "London"];
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>Not item found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
           <li
             onClick={() => {
               setSelectedIndex(index);
+              onSelectItem(item);
             }}
             className={
               selectedIndex === index
